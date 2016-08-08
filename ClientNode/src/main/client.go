d0d42e0a7386bb21062916e1173ce4c336660b2f
Justin Richard <justinr@uvic.ca>
2016-08-08 15:42:56 -0700

    updated client/server and chat

--- ClientNode/src/main/client.go
+++ ClientNode/src/main/client.go
@@ -1,41 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"net/rpc"
 )
 
-type Arith struct {
-	client *rpc.Client
+type AllowedFunctions interface {
+    Multiply(args *Args, reply *int) error
 }
 
 type Args struct {
 	A, B int
 }
 
-func (t *Arith) Multiply(a, b int) int {
-	args := &Args{a, b}
-	var reply int
-	err := t.client.Call("Arith.Multiply", args, &reply)
-	if err != nil {
-		log.Fatal("arith error:", err)
-	}
-	return reply
+type Arith int
+
+func (t *Arith) Multiply(args *Args, reply *int) error {
+	*reply = args.A * args.B
+	return nil
 }
 
 func main() {
 
-	// Tries to connect to localhost:1234 (The port on which rpc server is listening)
-	conn, err := net.Dial("tcp", "54.213.238.143:1337")
-	if err != nil {
-		log.Fatal("Connecting:", err)
+	//Creating an instance of struct which implement Arith interface
+	//allowedFunctions := new(AllowedFunctions)
+	arith := new(Arith)
+
+	// Register a new rpc server (In most cases, you will use default server only)
+	// And register struct we created above by name "Arith"
+	// The wrapper method here ensures that only structs which implement Arith interface
+	// are allowed to register themselves.
+	server := rpc.NewServer()
+	server.Register(arith)
+
+	// Listen for incoming tcp packets on specified port.
+	l, e := net.Listen("tcp", ":1337")
+	if e != nil {
+		log.Fatal("listen error:", e)
 	}
 
-	// Create a struct, that mimics all methods provided by interface.
-	// It is not compulsory, we are doing it here, just to simulate a traditional method call.
-	arith := &Arith{client: rpc.NewClient(conn)}
-
-	fmt.Println(arith.Multiply(5, 6))
+	// This statement links rpc server to the socket, and allows rpc server to accept
+	// rpc request coming from that socket.
+	server.Accept(l)
 }
