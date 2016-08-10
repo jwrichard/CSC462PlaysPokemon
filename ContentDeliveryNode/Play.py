fb4b17dcc32c9127ab16f8908edabd62fc975557
Justin Richard <justinr@uvic.ca>
2016-08-10 15:56:37 -0700

    Updated py script to get from network

--- ContentDeliveryNode/Play.py
+++ ContentDeliveryNode/Play.py
@@ -12,6 +12,12 @@ import win32api
 import win32con
 import win32com
 
+# Python 2.x
+import urllib2
+
+# Python 3.x 
+#import urllib.request
+
 # Up - W
 # Down - S
 # Left - A
@@ -23,8 +29,9 @@ import win32com
 def main():
     # Activate pokemon window
     while True:
-        move = getMove()
-        if move == 'up':
+        move = getMoveFromServer()
+        ms = 0x00;
+		if move == 'up':
             ms = 0x57
         if move == 'down':
             ms = 0x53
@@ -40,11 +47,11 @@ def main():
             ms = 0x5A
         if move == 'b':
             ms = 0x58
-        win32api.keybd_event(ms, 0)
-        time.sleep(0.05)
-        win32api.keybd_event(ms, 0 , 0x0002 , 0)
-
-        time.sleep(1)
+        if move != 0x00:
+			win32api.keybd_event(ms, 0)
+			time.sleep(0.05)
+			win32api.keybd_event(ms, 0 , 0x0002 , 0)
+        time.sleep(5)
 
 # Gets a random move for testing
 def getMove():
@@ -52,7 +59,8 @@ def getMove():
 
 # Gets a random move from the server - TODO
 def getMoveFromServer():
-    return "up"
+	return urllib2.urlopen("http://54.213.250.201/move").read() # Python 2.x
+    # return urllib.request.urlopen("http://54.213.250.201/move").read() # Python 3.x
 
 if __name__ == '__main__':
     main()
